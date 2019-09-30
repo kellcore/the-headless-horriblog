ba07b4398b8d74ec29513d66d30bf1b13003d0b8
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-09-30 17:29:40 -0400

    Added graphql query to gatsby-node.js to create individual blog pages from Contentful data and created templates folder with blog.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,53 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+    // query for nodes to use in creating pages
+    resolve(
+        graphql(request).then(result => {
+            if (result.errors) {
+                reject(result.errors)
+            }
+            return result;
+        })
+    )
+});
+
+// implement the Gatsby API "createPages" -> this is called once the data layer is bootstrapped to let plugins create pages from data
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions;
+
+    // create pages for each blog
+
+    const getBlog = makeRequest(graphql, `
+    {
+        allContentfulBlog(
+            sort: { fields: [createdAt], order: DESC }
+            filter: {
+                node_locale: {eq: "en-US"}
+            },
+        )
+        {
+            edges {
+                node {
+                    id
+                    slug
+                }
+            }
+        }
+    }
+`).then(result => {
+        result.data.allContentfulBlog.edges.forEach(({ node }) => {
+            createPage({
+                path: `blog/${node.slug}`,
+                component: path.resolve(`src/templates/blog.js`),
+                context: {
+                    id: node.id,
+                },
+            })
+        })
+    })
+    return Promise.all([
+        getBlog
+    ]);
+};
