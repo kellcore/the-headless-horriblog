560b8fd5e95170292a76a6d643c889e98b95220a
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-09-30 14:44:54 -0400

    Created home folder inside components, added graphQL to home/index.js, and imported Home component inside pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Nav from "../components/nav/index";
 import Featured from "../components/featured";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import Home from "../components/home";
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -9,6 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Nav />
     <Featured />
+    <Home />
   </Layout>
 )
 
