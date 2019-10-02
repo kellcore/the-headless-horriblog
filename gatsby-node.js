4a6ae832ee0b403c792b6b0e4faafad530b1539e
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-10-02 14:07:15 -0400

    Installed gatsby-transformer-remark, added BlogTemplate component and graphql query to blog.js, and created blog.css

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,7 +42,7 @@ exports.createPages = ({ actions, graphql }) => {
                 path: `blog/${node.slug}`,
                 component: path.resolve(`src/templates/blog.js`),
                 context: {
-                    id: node.id,
+                    id: node.id
                 },
             })
         })
@@ -51,3 +51,59 @@ exports.createPages = ({ actions, graphql }) => {
         getBlog
     ]);
 };
+// const path = require(`path`);
+
+// const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+//     // Query for nodes to use in creating pages.
+//     resolve(
+//         graphql(request).then(result => {
+//             if (result.errors) {
+//                 reject(result.errors)
+//             }
+//             return result;
+//         })
+//     )
+// });
+
+// // Implement the Gatsby API "createPages". This is called once the
+// // data layer is bootstrapped to let plugins create pages from data.
+// exports.createPages = ({ actions, graphql }) => {
+//     const { createPage } = actions;
+
+//     // Create pages for each blog.
+//     const getBlog = makeRequest(graphql, `
+//    {
+//      allContentfulBlog (
+//        sort: { fields: [createdAt], order: DESC }
+//        filter: {
+//          node_locale: {eq: "en-US"}})
+//      {
+//        edges {
+//          node {
+//            id
+//            slug
+//          }
+//        }
+//      }
+//    }
+//    `).then(result => {
+//         result.data.allContentfulBlog.edges.forEach(({ node }) => {
+//             createPage({
+//                 path: `blog/${node.slug}`,
+//                 component: path.resolve(`src/templates/blog.js`),
+//                 context: {
+//                     id: node.id
+//                 }
+//             })
+//         })
+//     });
+//     return Promise.all([
+//         getBlog
+//         // getArchive,
+//         // getTravel,
+//         // getGuide,
+//         // getOpinion,
+//         // getTech
+//     ])
+// };
+
