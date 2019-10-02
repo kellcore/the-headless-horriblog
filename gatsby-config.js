50a0abfef2eba70bb10ada2dbe0a50a5487b0c7b
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-10-02 14:56:40 -0400

    Changed title of blog and icon on gatsby-config.js and removed page-2.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Abbatoir of Awful`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -45,7 +45,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/LogoMakr_7TR2Nf.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
