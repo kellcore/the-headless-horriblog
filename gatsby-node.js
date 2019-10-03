de2418c408581956bb3dcd21798dfb5c47d8a9c5
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-10-03 15:40:53 -0400

    Added queries for Contemporary and Un Blocks to gatsby-node.js and created contemporary-block.js and un-block.js in src/templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,6 +46,7 @@ exports.createPages = ({ actions, graphql }) => {
             })
         })
     });
+
     //create archive page for all blogs (including pagination)
     const getArchive = makeRequest(graphql, `
         {
@@ -90,8 +91,100 @@ exports.createPages = ({ actions, graphql }) => {
         })
     });
 
+    // create contemporary block category page (including pagination)
+    const getContemporaryBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Contemporary Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/contemporary-block` : `category/contemporary-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/contemporary-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create un block category page (including pagination)
+    const getUnBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Un Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/un-block` : `category/un-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/un-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
     return Promise.all([
         getBlog,
-        getArchive
+        getArchive,
+        getContemporaryBlock,
+        getUnBlock
     ]);
 };
