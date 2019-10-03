36edeb7963ac299a4bd7fd62b261863fd0744c1c
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-10-03 16:14:52 -0400

    Added the remaining category queries to gatsby-node.js and created templates for all

--- gatsby-node.js
+++ gatsby-node.js
@@ -181,10 +181,332 @@ exports.createPages = ({ actions, graphql }) => {
         })
     });
 
+    // create vampire block category page (including pagination)
+    const getVampireBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Vampire Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/vampire-block` : `category/vampire-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/vampire-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create awful block category page (including pagination)
+    const getAwfulBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Awful Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/awful-block` : `category/awful-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/awful-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create slasher block category page (including pagination)
+    const getSlasherBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Slasher Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/slasher-block` : `category/slasher-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/slasher-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create japanese block category page (including pagination)
+    const getJapaneseBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Japanese Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/japanese-block` : `category/japanese-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/japanese-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create stephen king block category page (including pagination)
+    const getStephenKingBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "Stephen King Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/stephen-king-block` : `category/stephen-king-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/stephen-king-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create john carpenter block category page (including pagination)
+    const getJohnCarpenterBlock = makeRequest(graphql, `
+        {
+            allContentfulBlog(
+                sort: { fields: [createdAt], order: DESC }
+                filter: {
+                    node_locale: {eq: "en-US"}
+                    category: {elemMatch: {title: {eq: "John Carpenter Block"}}}
+                },
+            )
+            {
+                edges {
+                    node {
+                        id
+                        slug
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/john-carpenter-block` : `category/john-carpenter-block/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/john-carpenter-block.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
+    // create other category page (including pagination)
+    const getOther = makeRequest(graphql, `
+     {
+         allContentfulBlog(
+             sort: { fields: [createdAt], order: DESC }
+             filter: {
+                 node_locale: {eq: "en-US"}
+                 category: {elemMatch: {title: {eq: "Other"}}}
+             },
+         )
+         {
+             edges {
+                 node {
+                     id
+                     slug
+                 }
+             }
+         }
+     }
+ `).then(result => {
+        const blogs = result.data.allContentfulBlog.edges;
+        const blogsPerPage = 9;
+        // calculate how many pages we actually need
+        const numOfPages = Math.ceil(blogs.length / blogsPerPage);
+
+        Array.from({ length: numOfPages }).forEach((_, i) => {
+            // for each value that is in the array, create a page for it using the createPage API
+            createPage({
+                path: i === 0 ? `/category/other` : `category/other/${i + 1}`,
+                // is this the first thing? if so, set url path to /blog, otherwise, we look at the actual position of what's in the array -> add 1 to the value of i
+                // ex. blog/2
+                component: path.resolve("./src/templates/other.js"),
+                // we need to tell it where to pull in the template
+                context: {
+                    // context allows us to pass the data into the component so we can use it in the graphql query
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    // skip will allow us to go past the first 9 things when this query is called
+                    numOfPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    });
+
     return Promise.all([
         getBlog,
         getArchive,
         getContemporaryBlock,
-        getUnBlock
+        getUnBlock,
+        getVampireBlock,
+        getAwfulBlock,
+        getSlasherBlock,
+        getJapaneseBlock,
+        getStephenKingBlock,
+        getJohnCarpenterBlock,
+        getOther
     ]);
 };
---
+++ src/templates/stephen-king-block.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { Link, graphql, navigate } from 'gatsby';
+import { window } from 'browser-monads';
+import Layout from '../components/layout';
+import Nav from '../components/nav';
+import SEO from '../components/seo';
+import '../components/home/home.css';
+import './archive.css';
+
+import headerImg from '../images/mario-azzi-DY2miYwMchk-unsplash.jpg';
+
+const StephenKingBlock = (props) => {
+    const blogContent = props.data.allContentfulBlog;
+    const { currentPage, numOfPages } = props.pageContext;
+    // this comes from gatsby-node!
+    const isFirstPage = currentPage === 1;
+    const isLastPage = currentPage === numOfPages;
+    const previousPage = currentPage - 1 === 1 ? '/category/stephen-king-block' : `/category/stephen-king-block${currentPage - 1}`;
+    // does the current page - 1 equal 1? if so, direct to blog, otherwise, direct to blog/2 etc.
+    const nextPage = `/category/stephen-king-block/${currentPage + 1}`;
+
+    return (
+        <Layout>
+            <SEO title='Stephen King Block' keywords={['halloween', 'horror blog', 'horror movies']} />
+            <Nav />
+
+            <header>
+                <div className='archive_section'>
+                    <div className='archive_hero' style={{ backgroundImage: `url(${headerImg})` }}>
+                    </div>
+                    <div className="archive_nav">
+                        <Link to='/blog' className={window.location.href.indexOf('/blog') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> All </Link>
+                        <Link to='/category/contemporary-block' className={window.location.href.indexOf('category/contemporary-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Contemporary Block </Link>
+                        <Link to='/category/un-block' className={window.location.href.indexOf('category/un-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Un Block </Link>
+                        <Link to='/category/vampire-block' className={window.location.href.indexOf('category/vampire-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Vampire Block </Link>
+                        <Link to='/category/awful-block' className={window.location.href.indexOf('category/awful-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Awful Block </Link>
+                        <Link to='/category/slasher-block' className={window.location.href.indexOf('category/slasher-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Slasher Block </Link>
+                        <Link to='/category/japanese-block' className={window.location.href.indexOf('category/japanese-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Japanese Block </Link>
+                        <Link to='/category/stephen-king-block' className={window.location.href.indexOf('category/stephen-king-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Stephen King Block </Link>
+                        <Link to='/category/john-carpenter-block' className={window.location.href.indexOf('category/john-carpenter-block') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> John Carpenter Block </Link>
+                        <Link to='/category/other' className={window.location.href.indexOf('category/other') > 0 ? 'archive_nav_link selected' : 'archive_nav_link'}> Other </Link>
+                    </div>
+                </div>
+            </header>
+
+            <div className='feed'>
+                {blogContent.edges.map(edge => (
+                    <div key={edge.node.id} className='card'
+                        style={{
+                            backgroundImage: `linear-gradient(
+                            to bottom,
+                            rgba(10, 10, 10, 0) 0%,
+                            rgba(10, 10, 10, 0) 50%,
+                            rgba(10, 10, 10, 0.7) 100%),
+                            url(${edge.node.featuredImage.fluid.src})`
+                        }}
+                        onClick={() => navigate(`/blog/${edge.node.slug}`)}
+                    >
+                        {edge.node.category.map(category => (
+                            <p className='card_category'>{category.title}</p>
+                        ))}
+                        <p className="card_title">
+                            {edge.node.title}
+                        </p>
+                    </div>
+                ))}
+            </div>
+
+            <div className='pagination'>
+                <div className='pagination_item'>
+                    {!isFirstPage && (
+                        <Link to={previousPage} rel="previous">
+                            <div className="arrow_back">
+                            </div>
+                        </Link>
+                    )}
+                    {/* if this is not the first page and true, we display an arrow back */}
+                </div>
+                <div className='pagination_item'>
+                    {!isLastPage && (
+                        <Link to={nextPage} rel="next">
+                            <div className="arrow_next">
+                            </div>
+                        </Link>
+                    )}
+                    {/* if this is not the last page and true, we display an arrow forward */}
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default StephenKingBlock;
+
+export const pageQuery = graphql`
+    query StephenKingBlockQuery ($skip: Int!, $limit: Int!) {
+        allContentfulBlog(
+            sort: { fields: [createdAt], order: DESC }
+            filter: {
+                node_locale: {eq: "en-US"}
+                category: {elemMatch: {title: {eq: "Stephen King Block"}}}
+            }
+            skip: $skip
+            limit: $limit
+        ) {
+            edges {
+                node {
+                    id
+                    slug
+                    title
+                    createdAt
+                    category {
+                        title
+                        id
+                    }
+                    featuredImage {
+                        fluid(maxWidth: 1200, quality: 85) {
+                            src
+                            ...GatsbyContentfulFluid
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
